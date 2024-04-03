4f5879b4e1e9d7ebd7aa0d2007c6fa6acc691c26
Devansh-bharadwaj <devanshbharadwaj1@gmail.com>
2024-04-03 07:53:32 +0530

    language change

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -40,11 +42,34 @@ const Header = () => {
     return () => unscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  };
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-full bg-gradient-to-b from-black z-10">
       <img src={LOGO} alt="logo" className="w-48" />
       {user && (
         <div className="absolute right-2 top-3">
+          <select
+            className="px-4 py-2 m-4 rounded-lg bg-gray-900 text-white"
+            onChange={handleLanguageChange}
+          >
+            {SUPPORTED_LANGUAGES.map((lang) => (
+              <option key={lang.identifier} value={lang.identifier}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+          <button
+            className="bg-purple-500 text-white p-2 rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            GPT Search
+          </button>
           <button
             onClick={handleSignOut}
             className="rounded-lg bg-red-700 p-2 m-2 text-white"
